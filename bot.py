557f416a7978a84d4aee58b896ab6f7202fb958c
LORD-SANTINO <mikkyrose297@gmail.com>
2025-08-30 01:23:45 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -27,7 +27,7 @@ WHATSAPP_SUPPORT_EMAILS = [
 
 OWNER_ID = 7243305432
 
-OWNER_URL = "https://t.me/Exodus_tech1"
+OWNER_URL = "https://t.me/dax_channel01"
 CHANNEL_URL = "https://t.me/Exodustrust"
 BUG_BOT_URL = "https://t.me/YourBugBot"   # <--- Define properly
 CPANEL_URL = "https://your-cpanel-link.com"  # <--- Define properly
